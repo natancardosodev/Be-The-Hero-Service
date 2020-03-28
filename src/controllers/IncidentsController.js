987aeba3b37ab6fa6f8f6e18e7a6f1ff596f701e
Natan Cardoso <natancb7@gmail.com>
2020-03-27 21:09:35 -0300

    add - paginação dos incidents

--- src/controllers/IncidentsController.js
+++ src/controllers/IncidentsController.js
@@ -4,7 +4,16 @@ const tableName = 'incidents';
 
 module.exports = {
     async index(request, response) {
-        const incidents = await connection(tableName).select('*');
+        const { page = 1 } = request.query;
+
+        const [count] = await connection('incidents').count();
+
+        const incidents = await connection(tableName)
+            .limit(5)
+            .offset((page - 1) * 5)
+            .select('*');
+
+        response.header('X-Total-Count', count['count(*)']);
 
         return response.json(incidents);
     },
